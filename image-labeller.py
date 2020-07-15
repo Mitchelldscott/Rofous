280592fa514f1785c86c700d7b95693f6a81b5ec
Mitchell Scott <misc4432@colorado.edu>
2020-07-14 17:32:01 -0700

    updates

--- image-labeller.py
+++ image-labeller.py
@@ -10,5 +10,19 @@ def load_images(path, num_images):
 	return images
 
 
-images = load_images('feild-images/img', 65)
-plt.imshow(images[1])
+images = load_images('field-images/img', 63)
+for i, image in enumerate(images):
+	print(image.shape)
+	cv.imshow(f'Image {i}', image)
+	cv.waitKey()
+	
+	
+"""	with open('field-images/field-poi-labels.txt', 'a+') as f:
+	p0 = input('1: ')
+	p1 = input('2: ')
+	p2 = input('3: ')
+	p3 = input('4: ')
+	c2 = input('c1: ')
+	c1 = input('c2: ')
+	poi = input('mp: ')
+		f.write(f'{label[0]},{label[1]},{label[2]},{label[3]},{label[4]},{label[5]},{label[6]}\n')"""
---
+++ image-viewer.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(path, num_images):
+	images = []
+	for i in range(num_images):
+		images.append(cv.imread(path + f'{i}.jpg'))
+	print(f'{len(images)} Images loaded successfully!')
+	with open('field-images/field-poi-labels.txt', 'r') as f:
+		labels = f.read().split('\n')
+	return images, labels
+
+
+images, labels = load_data('field-images/img', 65)
+for i, image in enumerate(images):
+	for point in labels[i +1].split(':'):
+		if point == '':
+			continue
+		p_actual = point.split(',')
+		p = (int(p_actual[0]), int(p_actual[1]))
+		cv.circle(image, p, 10, color=3)
+	cv.imshow(f'Image {i}', image)
+	cv.waitKey()
