b4f09f2d0a0115567fcc8f7125e554a02d87c2b4
Mitchell Scott <misc4432@colorado.edu>
2020-07-14 21:45:12 -0700

    Data updates

--- image-viewer.py
+++ image-viewer.py
@@ -7,12 +7,12 @@ def load_data(path, num_images):
 	for i in range(num_images):
 		images.append(cv.imread(path + f'{i}.jpg'))
 	print(f'{len(images)} Images loaded successfully!')
-	with open('field-images/field-poi-labels.txt', 'r') as f:
+	with open('field-images-v1/field-poi-labels.txt', 'r') as f:
 		labels = f.read().split('\n')
 	return images, labels
 
 
-images, labels = load_data('field-images/img', 65)
+images, labels = load_data('field-images-v1/img', 630)
 for i, image in enumerate(images):
 	for point in labels[i +1].split(':'):
 		if point == '':
@@ -22,3 +22,5 @@ for i, image in enumerate(images):
 		cv.circle(image, p, 10, color=3)
 	cv.imshow(f'Image {i}', image)
 	cv.waitKey()
+
+  
