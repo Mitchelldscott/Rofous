a899f7bf041d5701184bcb61baecaff65d65d410
MithellScott <misc4432@colorado.edu>
2020-10-29 20:16:54 -0600

    Restructure in progress

---
+++ CMakeFiles/Rofous.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rofous.dir/test/AD_U_Test.cpp.o"
+  "CMakeFiles/Rofous.dir/src/main.cpp.o"
+  "Rofous.pdb"
+  "Rofous"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rofous.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Rofous.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rofous.dir/main.cpp.o"
+  "Rofous.pdb"
+  "Rofous"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rofous.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
