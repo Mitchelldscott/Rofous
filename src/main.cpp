1a23aeb01918110206655811c9daac03e962846a
Mitchell D Scott <misc4432@colorado.edu>
2021-05-23 13:01:48 -0600

    cleaning up workspace

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
 	int e_ctr = 0;
 
 	if(argc == 1)
-	{
 		cout << name << " Usage: no jobs specified" << endl;
-	}
+
 	for(int i = 1; i < argc; i++)
 	{
 		if(argv[i] == std::string("U-Test"))
 			run_tests();
 	}
+	
 	cout << name << " Finished All Jobs with " << e_ctr << " errors" << endl;
 	return 0;
 }
